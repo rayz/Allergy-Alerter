1ab386544283a5b24e0124544716dd1e8b6a7662
William Wu <ww2497@rit.edu>
2020-02-09 02:48:10 -0500

    Some more css done

--- script.js
+++ script.js
@@ -15,40 +15,43 @@ function query(){
   //var display_data = [];
   var outputDiv = document.getElementById("output");
   outputDiv.innerHTML = "";
-  for(i=0;i<product_data.length&&i<5; i++){
+  for(i=0;i<product_data.length&&i<10; i++){
     var info = product_info(product_data[i].sku);
     var parsedInfo = JSON.parse(info)
 
     var brand = parsedInfo.brand;
     var name = parsedInfo.name;
-    var ingredients = parsedInfo.ingredients;
+    var ingredients = parsedInfo.ingredients[0];
+    if(ingredients){
+      ingredients = ingredients.substring(0,ingredients.length-1);
 
-    /*display_data.push(parsedInfo.brand+", "+
-      parsedInfo.name+" </br>"+
-      parsedInfo.ingredients+" </br></br>");
+      /*display_data.push(parsedInfo.brand+", "+
+        parsedInfo.name+" </br>"+
+        parsedInfo.ingredients+" </br></br>");
 
-    /*display_data.push(parsedInfo.brand+" ")
-    display_data.push(parsedInfo.name+" ")
-    display_data.push("</br>")
-    display_data.push(parsedInfo.ingredients+" ")
-    display_data.push("</br></br>")*/
-    
-    var allergenInput = document.getElementById("allergen");
-    allergen = allergenInput.value;
-    
-    var indiv_ing = ingredients[0].split(", ");
-    if(indiv_ing.includes(allergen)){
-      outputDiv.innerHTML = outputDiv.innerHTML+
-        "<div class='allergen'>"+
-        brand+", "+
-        name+" </br>"+
-        ingredients+"</br></br>"
-    }else{
-      outputDiv.innerHTML = outputDiv.innerHTML+
-        "<div>"+
-        brand+", "+
-        name+" </br>"+
-        ingredients+"</br></br>"
+      /*display_data.push(parsedInfo.brand+" ")
+      display_data.push(parsedInfo.name+" ")
+      display_data.push("</br>")
+      display_data.push(parsedInfo.ingredients+" ")
+      display_data.push("</br></br>")*/
+      
+      var allergenInput = document.getElementById("allergen");
+      allergen = allergenInput.value;
+
+      var indiv_ing = ingredients.split(", ");
+      if(indiv_ing.includes(allergen)){
+        outputDiv.innerHTML = outputDiv.innerHTML+
+          "<div class='allergen'><div class='product'>"+
+          brand+", "+
+          name+"</div></br><div class = 'ingredients'>"+
+          ingredients+"</div></br>";
+      }else{
+        outputDiv.innerHTML = outputDiv.innerHTML+
+          "<div><div class='product'>"+
+          brand+", "+
+          name+"</div></br><div class = 'ingredients'>"+
+          ingredients+"</div></br>";
+      }
     }
   }
   var outputDiv = document.getElementById("output");
